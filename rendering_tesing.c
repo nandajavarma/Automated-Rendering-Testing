34fdfd494331cbf2a4c14eaf80c69637b82b0f4f
Nandaja Varma <nandaja.varma@gmail.com>
2013-06-25 23:35:07 +0530

    changing algo

--- rendering_tesing.c
+++ rendering_tesing.c
@@ -3,25 +3,24 @@
 
 int main(int argc, char *argv[])
 {
-  int i;
-  char buffer[20];
-  char original[1024] = {'\0'};
-  char testing[1024] = {'\0'};
+  int i = 0, j = 0, k = 0, m =0, n=0, l =0, opsize, tpsize;
+  char word[20][20], glyph[20][20];
   FILE *op, *tp, *rp;
   if(argc != 3)
     {
       printf("Usage: ./rendering_testing orig_glyph.txt rendered_glyphs.txt\n");
       exit(0);
     }
-  snprintf(original, 1024, "%s", argv[1]);
-  snprintf(testing, 1024, "%s", argv[2]);
-  op = fopen(original, "r");
-  tp = fopen(testing, "r");
+  op = fopen(argv[1], "r");
+  tp = fopen(argv[2], "r");
   rp = fopen("result.txt", "rw");
-  for(i = 0; buffer[i] != '['; i++)
-    {
-      fread(&buffer[i], 1, 1, op);
-    }
-  for(i = 0; i != '/0'; i++)
-    printf("%s", buffer[i]);
+  fseek(op, 0, SEEK_END); // seek to end of file
+  opsize = ftell(op); // get current file pointer
+  fseek(op, 0, SEEK_SET); // seek back to beginning of file
+  fseek(tp, 0, SEEK_END); // seek to end of file
+  tpsize = ftell(tp); // get current file pointer
+  fseek(tp, 0, SEEK_SET); // seek back to beginning of file
+  char bufferop[opsize], buffertp[tpsize];
+  fread(bufferop, opsize, 1, op);
+  fread(buffertp, tpsize, 1, tp);
 }
