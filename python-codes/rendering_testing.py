1208f89729c9a66ddf9d0d5fa923bcb2021a63a5
Nandaja Varma <nandaja.varma@gmail.com>
2013-07-16 23:35:16 +0530

    Modifying the test file

--- python-codes/rendering_testing.py
+++ python-codes/rendering_testing.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
-import os
 import sys
-import re
-
+import os
 def renderingtest(op, tp):
 	rfile = open("result.txt", 'w')
 	opfile = op.read()
@@ -37,39 +35,39 @@ def renderingtest(op, tp):
 		print "No rendering problems found"
 		return
 	else:
-		print "Rendering problems detected for the following words:\n"
+		print "Rendering problems detected and results are stored in the file result.txt\n"
 		for word in res:	
 			d = olist.index(word)
-			print wordlist[d]
 			rfile.write(wordlist[d] + '\n')
-	return rfile
+	rfile.close()
+	return 
     						
 def meera():
 	op = open("meera-glyph.txt")
 	ff = "Meera.ttf"
-	tp = open("harfbuzz_Meera.ttf.txt") 
-	print "The font you have selected is Meera"
+	tp = open("hb_meera_rendering.txt") 
+	print "The font you have selected is Meera\n"
 	return op, tp
 	
 def rachana():
 	op = open("rachana_glyph.txt")
 	ff = "Rachana.ttf"
-	tp = open("harfbuzz_Rachana.ttf.txt")
-	print "The font you have selected Rachana"
+	tp = open("hb_rachana_rendering.txt")
+	print "The font you have selected Rachana\n"
 	return op, tp
 
 def suruma(): 
 	op = open("suruma-glyph.txt")
 	ff = "Suruma.ttf"
-	tp = open("harfbuzz_Suruma.ttf.txt")
-	print "The font you have selected is Suruma"
+	tp = open("hb_suruma_rendering.txt")
+	print "The font you have selected is Suruma\n"
 	return op, tp
 
 def lohith(): 
 	op = open("lohith-glyph.txt")
 	ff = "Lohit-Malayalam.ttf"
-	tp = open("harfbuzz_Lohit-Malayalam.ttf.txt")
-	print "The font you have selected is Lohith"
+	tp = open("hb_lohit_rendering.txt")
+	print "The font you have selected is Lohit-Malyalam\n"
 	return op, tp
 
 def somel():
@@ -79,22 +77,24 @@ def somel():
 	op = open(reffile)
 	tp = open(rendfile)
 
-ch = int(raw_input("Welcome! Select the font for rendering testing.\n1. Meera\n2. Rachana\n3. Suruma\n4. Lohith\n"))
+ch = int(raw_input("Welcome! Select the font for rendering testing.\n1. Meera\n2. Rachana\n3. Suruma\n4. Lohit-Malayalam\n5. Something else\n"))
 choice = {1: meera, 2: rachana, 3: suruma,  4: lohith, 5: somel}
 wp = open("ml-test-cases.txt")
 
 op, tp = choice[ch]()
-resultop = renderingtest(op, tp)
+renderingtest(op, tp)
 
 if ch == 1:
-	os.system("hb-view Meera.ttf --text-file=result.txt >> output.png")
+	os.system('hb-view Meera.ttf --text-file=result.txt > output.png')
 elif ch == 2:
-  os.system('hb-view Rachana.ttf --text-file=result.txt >> output.png')
+  os.system('hb-view Rachana.ttf --text-file=result.txt > output.png')
 elif ch == 3:
-  os.system('hb-view Suruma.ttf --text-file=result.txt >> output.png')
+  os.system('hb-view Suruma.ttf --text-file=result.txt > output.png')
 elif ch == 4:
-	os.system('hb-view Lohit-Malayalam.ttf --text-file=result.txt >> output.png')
+	os.system('hb-view Lohit-Malayalam.ttf --text-file=result.txt > output.png')
 
-
+wp.close()
+op.close()
+tp.close()
 
 
