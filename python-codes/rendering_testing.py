1e2401228d6143fac82be28fe5bda65e67a64e44
Nandaja Varma <nandaja.varma@gmail.com>
2013-07-18 18:46:18 +0530

    cleaning up the code and test cases

--- python-codes/rendering_testing.py
+++ python-codes/rendering_testing.py
@@ -2,88 +2,94 @@
 import sys
 import os
 def renderingtest(op, tp):
-	rfile = open("result.txt", 'w')
-	opfile = op.read()
-	tpfile = tp.read()
-	olist = []
-	tlist = []
-	olines = opfile.replace('[','').replace(']','')
-	opsop = olines.split('\n')
-	for aline in opsop:
-		output = aline.split(',')		
-		olist.append(output)
-
-	linepart = tpfile.replace('[','').replace(']','') 
-	tpsop = linepart.split('\n')
-	for line in tpsop:
-		newpart = line.split('|')
-		tlist.append(newpart)
-	m = []
-	for x in tlist:
+	result_file = open("result.txt", 'w')
+	reference_file = op.read()
+	rendered_file = tp.read()
+	ref_list = []
+	rend_list = []
+	#Parsing the reference file
+	ref_lines = reference_file.replace('[','').replace(']','')
+	clean_file = ref_lines.split('\n')
+	for aline in clean_file:
+		aword = aline.split(',')		
+		ref_list.append(aword)
+	#Parsing the harfbuzz renderings' file
+	rend_lines = rendered_file.replace('[','').replace(']','') 
+	rend_file_lines = rend_lines.split('\n')
+	for line in rend_file_lines:
+		each_name = line.split('|')
+		rend_list.append(each_name)
+	rendered_output = []
+	for x in rend_list:
 		n = []
 		for y in x:
 			n.append(y.split('=')[0])
-		m.append(n)
-
-	wordfile = wp.read()
+		rendered_output.append(n)
+	#Opening the file with test cases
+	wordfile_content = wp.read()
 	wordlist = []
-	wordlist = wordfile.split('\n')
-	
-	res = []
-	res = [i for i, j in zip(olist, m) if i != j]
-	if res == []:
+	wordlist = wordfile_content.split('\n')
+	#Matching rendered glyph names with the reference glyph names
+	result_list = []
+	result_list = [i for i, j in zip(ref_list, rendered_output) if i != j]
+	if result_list == []:
 		print "No rendering problems found"
 		return
 	else:
-		print "Rendering problems detected and results are stored in the file result.txt\n"
-		for word in res:	
-			d = olist.index(word)
-			rfile.write(wordlist[d] + '\n')
-	rfile.close()
+		print "Rendering problems detected. Wrongly rendered texts can be found inside result.txt or output.png\n"
+	#Finding the wrongly rendered words from the test cases file and writing it to result.txt
+		for word in result_list:	
+			d = ref_list.index(word)
+			result_file.write(wordlist[d] + '\n')
+	result_file.close()
 	return 
-    						
+#Defining reference files for font Meera    						
 def meera():
 	op = open("meera-glyph.txt")
 	ff = "Meera.ttf"
 	tp = open("hb_meera_rendering.txt") 
+	wp = open("ml-test-cases.txt")
 	print "The font you have selected is Meera\n"
 	return op, tp
-	
+#Defining reference files for font Rachana		
 def rachana():
 	op = open("rachana_glyph.txt")
 	ff = "Rachana.ttf"
 	tp = open("hb_rachana_rendering.txt")
+	wp = open("ml-test-cases.txt")
 	print "The font you have selected Rachana\n"
 	return op, tp
-
+#Defining reference files for font Suruma
 def suruma(): 
 	op = open("suruma-glyph.txt")
 	ff = "Suruma.ttf"
 	tp = open("hb_suruma_rendering.txt")
+	wp = open("ml-test-cases.txt")
 	print "The font you have selected is Suruma\n"
 	return op, tp
-
+#Defining reference files for font Lohit-Malayalam
 def lohith(): 
 	op = open("lohith-glyph.txt")
 	ff = "Lohit-Malayalam.ttf"
 	tp = open("hb_lohit_rendering.txt")
+	wp = open("ml-test-cases.txt")
 	print "The font you have selected is Lohit-Malyalam\n"
 	return op, tp
-
+#Leaving an option if the tester want to give the font and the rest of the files herself
 def somel():
 	wordfile = raw_input("\nEnter the name of the reference words file:")
 	reffile  = raw_input("\nEnter its correcponding glyph names' file:")
 	rendfile = raw_input("\nEnter the name of the file containing the harfbuzz renderings:")
 	op = open(reffile)
+	wp = open(wordfile)
 	tp = open(rendfile)
 
 ch = int(raw_input("Welcome! Select the font for rendering testing.\n1. Meera\n2. Rachana\n3. Suruma\n4. Lohit-Malayalam\n5. Something else\n"))
 choice = {1: meera, 2: rachana, 3: suruma,  4: lohith, 5: somel}
-wp = open("ml-test-cases.txt")
 
 op, tp = choice[ch]()
 renderingtest(op, tp)
-
+#Showing the results as the output of hb-view command in output for png
 if ch == 1:
 	os.system('hb-view Meera.ttf --text-file=result.txt > output.png')
 elif ch == 2:
