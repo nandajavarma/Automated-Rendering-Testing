170edb0dba5333007da7a156433c30685c59985b
Nandaja Varma <nandaja.varma@gmail.com>
2013-08-26 05:26:04 +0530

    modifying interface

--- web_app/app/routes.py
+++ web_app/app/routes.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template
+import os
  
 app = Flask(__name__)      
  
 @app.route('/')
 def home():
   return render_template('home.html')
+
+@app.route('/test', methods=['POST'])
+def test():
+	ff = request.form['fontfile']
+	wf = request.form['testcases']
+	rf = request.form['referencefile']
+	rendf = request.form['renderedoutput']
+	outfile = request.form['outputfile']
+	errorfile = request.form['errorfile']
+	mydir = request.form['directory']
+	os.system('./renderingtesting.py -w wf -r rf it rendf -o outfile -e errorfile -f ff -m mydir')
+	return
 
 @app.route('/about')
 def about():
