1b9f8e412998c642de7f0d900b183cdd4b034f17
Nandaja Varma <nandaja.varma@gmail.com>
2013-09-22 15:20:37 +0530

    Modifying code to work when files are not provided

--- rendering_test.py
+++ rendering_test.py
@@ -39,8 +39,9 @@ test_cases = ConfigSectionMap('test-cases-file')
 output_file = ConfigSectionMap('output-file')
 engine = ConfigSectionMap('shaping-engine')
 hbflag = 0
-if engine.lower() == 'harfbuzz':
-    hbflag = 1
+if engine:
+    if engine.lower() == 'harfbuzz':
+        hbflag = 1
 outflag = 0
 if output_file:
     if output_file.endswith('.html'):
@@ -61,38 +62,38 @@ if f == 1:
         print "Provide an html file as output file." + \
             "Cannot generate the result. Exiting.."
         sys.exit()
-    # Assuming the engine would be harfbuzz if a directory name is provided
-    dirname = 'hb_images'
-    diflag = 0
-    if hbflag and outflag:
-        if font_file:
-            if font_file.endswith('.ttf'):
-                diflag = 1
-                font_fp = open_file(font_file, "r")
-                if os.path.isdir(dirname):
-                    cmd0 = 'rm -rf ' + dirname
-                    os.system(cmd0)
-                cmd1 = 'mkdir ' + dirname
-                os.system(cmd1)
-                i = 0
-                print "\nProcessing...."
-                while wordlist[i] != "":
-                    cmd2 = 'hb-view ' + font_file + ' ' + \
-                        wordlist[i] + ' > ' + dirname + \
-                        '/' + '%d' % (i + 1) + '.png'
-                    os.system(cmd2)
-                    i = i + 1
-                font_fp.close()
-            else:
-                print "Provide a font file with .ttf extension." + \
-                    "Cannot create harfbuzz rendered images.\n"
-        else:
-            print "No font file provided!" + \
-                "Cannot create harfbuzz rendered images.\n"
     if outflag:
         if test_cases:
+            #creating images if the engine is harfbuzz
+            dirname = 'hb_images'
+            diflag = 0
+            if hbflag:
+                if font_file:
+                    if font_file.endswith('.ttf'):
+                        diflag = 1
+                        font_fp = open_file(font_file, "r")
+                        if os.path.isdir(dirname):
+                            cmd0 = 'rm -rf ' + dirname
+                            os.system(cmd0)
+                        cmd1 = 'mkdir ' + dirname
+                        os.system(cmd1)
+                        i = 0
+                        print "\nProcessing...."
+                        while wordlist[i] != "":
+                            cmd2 = 'hb-view ' + font_file + ' ' + \
+                                wordlist[i] + ' > ' + dirname + \
+                                '/' + '%d' % (i + 1) + '.png'
+                            os.system(cmd2)
+                            i = i + 1
+                        font_fp.close()
+                    else:
+                        print "Provide a font file with .ttf extension." + \
+                            "Cannot create harfbuzz rendered images.\n"
+                else:
+                    print "No font file provided!" + \
+                        "Cannot create harfbuzz rendered images.\n"
             print "\nOpen the file '" + output_file + \
-                "' in a browser to see the result\n"
+                        "' in a browser to see the result\n"
             # calling function to generate the results file
             testing_modules.get_result(a, wordlist, output_fp, diflag, dirname)
             output_fp.close()
