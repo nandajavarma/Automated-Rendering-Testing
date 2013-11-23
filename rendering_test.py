ca761ba4b344d3c57383db820a9b4e0d141410c0
Nandaja Varma <nandaja.varma@gmail.com>
2013-11-23 09:20:52 +0530

    Cleaning up the rendering_test script
    Changing func name in rendering_test.py
    
    Cleaning up rendering_test.py
    
    fixing pep8 errors

--- rendering_test.py
+++ rendering_test.py
@@ -23,21 +23,9 @@ import os
 import testing_modules
 from array import *
 
-try:
-    config_file = sys.argv[1]
-    if config_file.endswith('.ini') == 0:
-        print "Provide a configuration file in ini format as the \
-            argument. Exiting!.."
-        sys.exit()
-except IndexError:
-    print "Usage: rendering_test.py CONFIGURATION_FILE\
-        \nProvide an ini file as argument"
-    sys.exit()
-
-# Checking error status of files
-
 
 def get_path(file_name):
+    # Checking error status of files
     config_dir = os.path.dirname(config_file)
     file_dir = os.path.dirname(file_name)
     if file_dir:
@@ -65,90 +53,114 @@ def ConfigSectionMap(option):
         file_name = 0
     return file_name
 
-Config = ConfigParser.ConfigParser()
-Config.read(config_file)
 
-ref_file = ConfigSectionMap('reference-file')
-rend_output = ConfigSectionMap('rendered-output')
-if ref_file == 0 or rend_output == 0:
-    print "No refernce file or rendered output provided.. Exiting.."
-    sys.exit()
-font_file = ConfigSectionMap('font-file')
-test_cases = ConfigSectionMap('test-cases-file')
-output_file = ConfigSectionMap('output-file')
-engine = ConfigSectionMap('shaping-engine')
-hbflag = 0
-if engine:
-    if engine.lower() == 'harfbuzz':
-        hbflag = 1
-outflag = 0
-if output_file:
-    if output_file.endswith('.html'):
-        outflag = 1
-        output_fp = open_file(output_file, "w")
-if test_cases:
-    test_fp = open_file(test_cases, "r")
-else:
-    test_fp = 0
-ref_fp = open_file(ref_file, "r")
-rend_fp = open_file(rend_output, "r")
-# Calling function to test the engine
-a, wordlist, check_flag, ref_file_lines, rend_file_lines = \
-    testing_modules.main(ref_fp, rend_fp, test_fp)
-ref_pointer = open_file(ref_file, "r")
-rend_pointer = open_file(rend_output, "r")
-rend_lists = rend_pointer.read().split("\n")
-ref_lists = ref_pointer.read().split("\n")
-if check_flag == 1:
-    print "\nRendering problems observed!\n"
-    if output_file and outflag != 1:
-        print "Provide an html file as output file." + \
-            "Cannot generate the result. Exiting.."
-        sys.exit()
-    if outflag:
-        if test_cases:
-            # creating images if the engine is harfbuzz
-            dirname = get_path('hb_images')
-            diflag = 0
-            if hbflag:
-                if font_file:
-                    if font_file.endswith('.ttf'):
-                        diflag = 1
-                        font_fp = open_file(font_file, "r")
-                        if os.path.isdir(dirname):
-                            cmd0 = 'rm -rf ' + dirname
-                            os.system(cmd0)
-                        cmd1 = 'mkdir ' + dirname
-                        os.system(cmd1)
-                        i = 0
-                        print "\nProcessing...."
-                        font_path = get_path(font_file)
-                        while wordlist[i] != "":
-                            cmd2 = 'hb-view ' + font_path + ' ' + \
-                                wordlist[i] + ' > ' + dirname + \
-                                '/' + '%d' % (i + 1) + '.png'
-                            os.system(cmd2)
-                            i = i + 1
-                        font_fp.close()
+def rendering_check(ref_file, rend_output,
+                    font_file, test_cases, output_file, engine):
+    hbflag = 0
+    if engine:
+        if engine.lower() == 'harfbuzz':
+            hbflag = 1
+    outflag = 0
+    if output_file:
+        if output_file.endswith('.html'):
+            outflag = 1
+            output_fp = open_file(output_file, "w")
+    if test_cases:
+        test_fp = open_file(test_cases, "r")
+    else:
+        test_fp = 0
+    ref_fp = open_file(ref_file, "r")
+    rend_fp = open_file(rend_output, "r")
+    # Calling function to test the engine
+    a, wordlist, check_flag, ref_file_lines, rend_file_lines = \
+        testing_modules.main(ref_fp, rend_fp, test_fp)
+    ref_pointer = open_file(ref_file, "r")
+    rend_pointer = open_file(rend_output, "r")
+    rend_lists = rend_pointer.read().split("\n")
+    ref_lists = ref_pointer.read().split("\n")
+    if check_flag == 1:
+        print "\nRendering problems observed!\n"
+        if output_file and outflag != 1:
+            print "Provide an html file as output file." + \
+                "Cannot generate the result. Exiting.."
+            sys.exit()
+        if outflag:
+            if test_cases:
+                # creating images if the engine is harfbuzz
+                dirname = get_path('hb_images')
+                diflag = 0
+                if hbflag:
+                    if font_file:
+                        if font_file.endswith('.ttf'):
+                            diflag = 1
+                            font_fp = open_file(font_file, "r")
+                            if os.path.isdir(dirname):
+                                cmd0 = 'rm -rf ' + dirname
+                                os.system(cmd0)
+                            cmd1 = 'mkdir ' + dirname
+                            os.system(cmd1)
+                            i = 0
+                            print "\nProcessing...."
+                            font_path = get_path(font_file)
+                            while wordlist[i] != "":
+                                cmd2 = 'hb-view ' + font_path + ' ' + \
+                                    wordlist[i] + ' > ' + dirname + \
+                                    '/' + '%d' % (i + 1) + '.png'
+                                os.system(cmd2)
+                                i = i + 1
+                            font_fp.close()
+                        else:
+                            print "Provide a font file with .ttf extension.\
+                                Cannot create harfbuzz rendered images.\n"
                     else:
-                        print "Provide a font file with .ttf extension." + \
+                        print "No font file provided!" + \
                             "Cannot create harfbuzz rendered images.\n"
-                else:
-                    print "No font file provided!" + \
-                        "Cannot create harfbuzz rendered images.\n"
-            print "\nOpen the file '" + output_file + \
-                "' in a browser to see the result\n"
-            # calling function to generate the results file
-            testing_modules.get_result(
-                a,
-                wordlist,
-                output_fp,
-                diflag,
-                dirname,
-                ref_file_lines,
-                rend_file_lines)
-            output_fp.close()
-        else:
-            print "No test cases file provided! Cannot generate the result.\n"
-rend_fp.close()
-ref_fp.close()
+                print "\nOpen the file '" + output_file + \
+                    "' in a browser to see the result\n"
+                # calling function to generate the results file
+                testing_modules.get_result(
+                    a,
+                    wordlist,
+                    output_fp,
+                    diflag,
+                    dirname,
+                    ref_file_lines,
+                    rend_file_lines)
+                output_fp.close()
+            else:
+                print "No test cases file provided! Cannot generate \
+                    the result.\n"
+    rend_fp.close()
+    ref_fp.close()
+    return 0
+
+
+if __name__ == '__main__':
+    try:
+        config_file = sys.argv[1]
+        if config_file.endswith('.ini') == 0:
+            print "Provide a configuration file in ini format as the \
+                argument. Exiting!.."
+            sys.exit()
+    except IndexError:
+        print "Usage: rendering_test.py CONFIGURATION_FILE\
+            \nProvide an ini file as argument"
+        sys.exit()
+    Config = ConfigParser.ConfigParser()
+    Config.read(config_file)
+    ref_file = ConfigSectionMap('reference-file')
+    rend_output = ConfigSectionMap('rendered-output')
+    if ref_file == 0 or rend_output == 0:
+        print "No refernce file or rendered output provided.. Exiting.."
+        sys.exit()
+    font_file = ConfigSectionMap('font-file')
+    test_cases = ConfigSectionMap('test-cases-file')
+    output_file = ConfigSectionMap('output-file')
+    engine = ConfigSectionMap('shaping-engine')
+    rendering_check(
+        ref_file,
+        rend_output,
+        font_file,
+        test_cases,
+        output_file,
+        engine)
